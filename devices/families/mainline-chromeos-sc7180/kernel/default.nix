de58b18dac6d62c6e4113b8358286c0f9ba1f7a5
leo60228 <leo@60228.dev>
2026-02-19 17:38:24 -0500

    pin fetchpatch commit

--- devices/families/mainline-chromeos-sc7180/kernel/default.nix
+++ devices/families/mainline-chromeos-sc7180/kernel/default.nix
@@ -14,7 +14,7 @@ mobile-nixos.kernel-builder {
   patches = [
     # HACK: clk: Delay disabling unused clocks by 10s
     (fetchpatch {
-      url = "https://gitlab.postmarketos.org/postmarketOS/pmaports/-/raw/master/device/community/linux-postmarketos-qcom-sc7180/0011-HACK-clk-Delay-disabling-unused-clocks-by-10s.patch";
+      url = "https://gitlab.postmarketos.org/postmarketOS/pmaports/-/raw/972d39afe9077b06cd26475b4cce5dd7cdab00b2/device/community/linux-postmarketos-qcom-sc7180/0011-HACK-clk-Delay-disabling-unused-clocks-by-10s.patch";
       hash = "sha256-hOo0T9UmpwJxSxvkaIujEPTd8dlj1KRd7jMY8ZC8ito=";
     })
   ];
